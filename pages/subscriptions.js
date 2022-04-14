8dbaf5193f92e3c230eb6f69fc0e9c3119331f80
marywang@usc.edu <marywang@usc.edu>
2022-04-13 23:44:48 -0700

    finishing-ish subscriptions page

--- pages/subscriptions.js
+++ pages/subscriptions.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Button from "@mui/material/Button";
+
+// so much inline styling ill fix later T-T
 
 export default function Subscriptions() {
   return (
@@ -12,6 +15,19 @@ export default function Subscriptions() {
         position: "relative",
       }}
     >
+      <Button
+        variant="text"
+        size="large"
+        style={{
+          color: "black",
+          position: "absolute",
+          right: "10%",
+          top: "3%",
+          background: "#E1E1E1",
+        }}
+      >
+        CREATE A GROUP +
+      </Button>
       <div
         style={{
           width: "50%",
@@ -72,69 +88,377 @@ export default function Subscriptions() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          justifyContent: "space-between",
           position: "absolute",
           top: "5%",
+          height: "100%",
         }}
       >
         <div className="subscription">
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
               width: "40%",
+              paddingLeft: "4%",
+              position: "relative",
+              height: "100%",
             }}
           >
-            <h3 style={{ margin: "1%" }}>Service</h3>
-            <p style={{ margin: "1%" }}>Username</p>
-            <p style={{ margin: "1%" }}>Expiration Date:</p>
-            <p style={{ margin: "1%" }}>#/# Subscribers on this plan</p>
+            <h3
+              style={{ position: "absolute", top: "10%" }}
+              className="subscriptionPageLabels1"
+            >
+              Service
+            </h3>
+            <p
+              style={{ position: "absolute", top: "30%" }}
+              className="subscriptionPageLabels1"
+            >
+              Username
+            </p>
+            <p
+              style={{ position: "absolute", top: "40%" }}
+              className="subscriptionPageLabels1"
+            >
+              Expiration Date:
+            </p>
+            <p
+              style={{ position: "absolute", bottom: "10%", width: "100%" }}
+              className="subscriptionPageLabels1"
+            >
+              #/# Subscribed to this Plan
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p
+              style={{
+                position: "absolute",
+                top: "30%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              Current Rate:
+            </p>
+            <p
+              style={{
+                position: "absolute",
+                top: "40%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              If you joined...
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p style={{ position: "absolute", top: "30%", right: "10%" }}>
+              $5 per month
+            </p>
+            <p style={{ position: "absolute", top: "40%", right: "10%" }}>
+              $4.26 per month
+            </p>
+            <Button
+              variant="contained"
+              disableElevation
+              style={{
+                background: "#A4A1A1",
+                fontSize: "50%",
+                paddingLeft: "15%",
+                paddingRight: "15%",
+                position: "absolute",
+                bottom: "12%",
+                right: "10%",
+              }}
+            >
+              JOIN GROUP
+            </Button>
           </div>
         </div>
         <div className="subscription">
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
               width: "40%",
+              paddingLeft: "4%",
+              position: "relative",
+              height: "100%",
             }}
           >
-            <h3 style={{ margin: "1%" }}>Service</h3>
-            <p style={{ margin: "1%" }}>Username</p>
-            <p style={{ margin: "1%" }}>Expiration Date:</p>
-            <p style={{ margin: "1%" }}>#/# Subscribers on this plan</p>
+            <h3
+              style={{ position: "absolute", top: "10%" }}
+              className="subscriptionPageLabels1"
+            >
+              Service
+            </h3>
+            <p
+              style={{ position: "absolute", top: "30%" }}
+              className="subscriptionPageLabels1"
+            >
+              Username
+            </p>
+            <p
+              style={{ position: "absolute", top: "40%" }}
+              className="subscriptionPageLabels1"
+            >
+              Expiration Date:
+            </p>
+            <p
+              style={{ position: "absolute", bottom: "10%", width: "100%" }}
+              className="subscriptionPageLabels1"
+            >
+              #/# Subscribed to this Plan
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p
+              style={{
+                position: "absolute",
+                top: "30%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              Current Rate:
+            </p>
+            <p
+              style={{
+                position: "absolute",
+                top: "40%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              If you joined...
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p style={{ position: "absolute", top: "30%", right: "10%" }}>
+              $5 per month
+            </p>
+            <p style={{ position: "absolute", top: "40%", right: "10%" }}>
+              $4.26 per month
+            </p>
+            <Button
+              variant="contained"
+              disableElevation
+              style={{
+                background: "#A4A1A1",
+                fontSize: "50%",
+                paddingLeft: "15%",
+                paddingRight: "15%",
+                position: "absolute",
+                bottom: "12%",
+                right: "10%",
+              }}
+            >
+              JOIN GROUP
+            </Button>
           </div>
         </div>
         <div className="subscription">
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
               width: "40%",
+              paddingLeft: "4%",
+              position: "relative",
+              height: "100%",
             }}
           >
-            <h3 style={{ margin: "1%" }}>Service</h3>
-            <p style={{ margin: "1%" }}>Username</p>
-            <p style={{ margin: "1%" }}>Expiration Date:</p>
-            <p style={{ margin: "1%" }}>#/# Subscribers on this plan</p>
+            <h3
+              style={{ position: "absolute", top: "10%" }}
+              className="subscriptionPageLabels1"
+            >
+              Service
+            </h3>
+            <p
+              style={{ position: "absolute", top: "30%" }}
+              className="subscriptionPageLabels1"
+            >
+              Username
+            </p>
+            <p
+              style={{ position: "absolute", top: "40%" }}
+              className="subscriptionPageLabels1"
+            >
+              Expiration Date:
+            </p>
+            <p
+              style={{ position: "absolute", bottom: "10%", width: "100%" }}
+              className="subscriptionPageLabels1"
+            >
+              #/# Subscribed to this Plan
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p
+              style={{
+                position: "absolute",
+                top: "30%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              Current Rate:
+            </p>
+            <p
+              style={{
+                position: "absolute",
+                top: "40%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              If you joined...
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p style={{ position: "absolute", top: "30%", right: "10%" }}>
+              $5 per month
+            </p>
+            <p style={{ position: "absolute", top: "40%", right: "10%" }}>
+              $4.26 per month
+            </p>
+            <Button
+              variant="contained"
+              disableElevation
+              style={{
+                background: "#A4A1A1",
+                fontSize: "50%",
+                paddingLeft: "15%",
+                paddingRight: "15%",
+                position: "absolute",
+                bottom: "12%",
+                right: "10%",
+              }}
+            >
+              JOIN GROUP
+            </Button>
           </div>
         </div>
         <div className="subscription">
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
               width: "40%",
+              paddingLeft: "4%",
+              position: "relative",
+              height: "100%",
             }}
           >
-            <h3 style={{ margin: "1%" }}>Service</h3>
-            <p style={{ margin: "1%" }}>Username</p>
-            <p style={{ margin: "1%" }}>Expiration Date:</p>
-            <p style={{ margin: "1%" }}>#/# Subscribers on this plan</p>
+            <h3
+              style={{ position: "absolute", top: "10%" }}
+              className="subscriptionPageLabels1"
+            >
+              Service
+            </h3>
+            <p
+              style={{ position: "absolute", top: "30%" }}
+              className="subscriptionPageLabels1"
+            >
+              Username
+            </p>
+            <p
+              style={{ position: "absolute", top: "40%" }}
+              className="subscriptionPageLabels1"
+            >
+              Expiration Date:
+            </p>
+            <p
+              style={{ position: "absolute", bottom: "10%", width: "100%" }}
+              className="subscriptionPageLabels1"
+            >
+              #/# Subscribed to this Plan
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p
+              style={{
+                position: "absolute",
+                top: "30%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              Current Rate:
+            </p>
+            <p
+              style={{
+                position: "absolute",
+                top: "40%",
+                left: "80%",
+                width: "100%",
+              }}
+            >
+              If you joined...
+            </p>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <p style={{ position: "absolute", top: "30%", right: "10%" }}>
+              $5 per month
+            </p>
+            <p style={{ position: "absolute", top: "40%", right: "10%" }}>
+              $4.26 per month
+            </p>
+            <Button
+              variant="contained"
+              disableElevation
+              style={{
+                background: "#A4A1A1",
+                fontSize: "50%",
+                paddingLeft: "15%",
+                paddingRight: "15%",
+                position: "absolute",
+                bottom: "12%",
+                right: "10%",
+              }}
+            >
+              JOIN GROUP
+            </Button>
           </div>
         </div>
       </div>
