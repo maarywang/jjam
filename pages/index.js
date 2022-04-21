0083bf2342161db348a90a25deac3e925709a85b
marywang@usc.edu <marywang@usc.edu>
2022-04-21 01:52:33 -0700

    fixing next/image import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import Image from "next/Image";
+import Image from "next/image";
 
 export default function Home() {
   return (
