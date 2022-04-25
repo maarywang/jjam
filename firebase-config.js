d2549397ed020d5b6a586d785a64174adbd331c7
marywang@usc.edu <marywang@usc.edu>
2022-04-25 00:54:24 -0700

    secret stuff

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ import "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOz_b1GFeyaW_i3GkoVRbiEhJkt0xwets",
-  authDomain: "jjam-submerge.firebaseapp.com",
-  projectId: "jjam-submerge",
-  storageBucket: "jjam-submerge.appspot.com",
-  messagingSenderId: "588816949443",
-  appId: "1:588816949443:web:3acdb352fdfcbd28aa8276",
-  measurementId: "G-9H8HK2WT0B",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDING_ID,
+  appId: process.env.NEXT_PUBLIC_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASURMENT_ID,
 };
 
 let app;
